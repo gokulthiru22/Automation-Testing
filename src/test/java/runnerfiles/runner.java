d23d9af9a5af412f8b26a5c205d891bfed0f6099
gokulthiru22 <49223444+gokulthiru22@users.noreply.github.com>
2021-11-15 13:26:01 +0530

    Update runner.java

--- src/test/java/runnerfiles/runner.java
+++ src/test/java/runnerfiles/runner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = {"src/test/resources/featurefile/hirepros_CommonLogin.feature", "src/test/resources/featurefile/hirepros_Configurations.feature"},
 glue = "Stepdefinitions", plugin = {
-		"pretty", "html:target/htmlreports","json:target/cucumber-reports/Cucumber.json"})
+		"pretty", "html:target/htmlreports","json:target/Cucumber.json"})
 
 
 
